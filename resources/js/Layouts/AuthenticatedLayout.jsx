59bf9f6fdf5de5c48296770d2829348f367e2f35
JesselZapanta <jesselzapanta9@gmail.com>
2025-01-02 12:01:37 +0800

    update layout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,107 +1,137 @@
-import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-const { Header, Content, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
-const App = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-  return (
-      <Layout className="min-h-[100vh]">
-          <Header
-              style={{
-                  display: "flex",
-                  alignItems: "center",
-              }}
-          >
-              <div className="w-full flex items-center justify-between gap-4">
-                  <div>
-                      <img src="/images/dmo.png" className="w-16" alt="" />
-                  </div>
-                  <Menu
-                      theme="dark"
-                      mode="horizontal"
-                      defaultSelectedKeys={["2"]}
-                      items={items1}
-                      style={{
-                          flex: 1,
-                          minWidth: 0,
-                      }}
-                  />
-              </div>
-          </Header>
-          <Layout>
-              <Sider
-                  width={200}
-                  style={{
-                      background: colorBgContainer,
-                  }}
-              >
-                  <Menu
-                      mode="inline"
-                      defaultSelectedKeys={["1"]}
-                      defaultOpenKeys={["sub1"]}
-                      style={{
-                          height: "100%",
-                          borderRight: 0,
-                      }}
-                      items={items2}
-                  />
-              </Sider>
-              <Layout
-                  style={{
-                      padding: "0 24px 24px",
-                  }}
-              >
-                  <Breadcrumb
-                      items={[
-                          {
-                              title: "Home",
-                          },
-                          {
-                              title: "List",
-                          },
-                          {
-                              title: "App",
-                          },
-                      ]}
-                      style={{
-                          margin: "16px 0",
-                      }}
-                  />
-                  <Content
-                      style={{
-                          padding: 24,
-                          margin: 0,
-                          minHeight: 280,
-                          background: colorBgContainer,
-                          borderRadius: borderRadiusLG,
-                      }}
-                  >
-                      Content
-                  </Content>
-              </Layout>
-          </Layout>
-      </Layout>
-  );
-};
-export default App;
+import React, { useState } from "react";
+import {
+    AppstoreOutlined,
+    BankOutlined,
+    DashboardOutlined,
+    DownOutlined,
+    LineChartOutlined,
+    LogoutOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    ProjectOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from "@ant-design/icons";
+import { Avatar, Breadcrumb, Button, Dropdown, Layout, Menu, Space, theme } from "antd";
+import { Link } from "@inertiajs/react";
+import { Footer } from "antd/es/layout/layout";
+const { Header, Sider, Content } = Layout;
+export default function AuthenticatedLayout({ auth, page, children }) {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const currentRoute = route().current();
+
+    // Admin
+    const adminLinks = [
+        {
+            label: <Link href={route("dashboard")}>Dashboard</Link>,
+            key: "admin.dashboard",
+            icon: <DashboardOutlined />,
+        },
+    ];
+    return (
+        <Layout>
+            <Sider
+                trigger={null}
+                theme="light"
+                collapsible
+                breakpoint="lg"
+                collapsed={collapsed}
+                onCollapse={(collapsedValue) => setCollapsed(collapsedValue)}
+                className="m-h-[100vh] px-4"
+            >
+                <div className="w-full h-16 flex gap-2 items-center justify-center">
+                    <img
+                        className="w-16"
+                        src="/images/dmo.png"
+                        alt="DMO"
+                    />
+                </div>
+
+                {auth.user.role === 0 && (
+                    <Menu
+                        theme="light"
+                        mode="inline"
+                        defaultSelectedKeys={["1"]}
+                        selectedKeys={[currentRoute]}
+                        items={adminLinks}
+                    />
+                )}
+            </Sider>
+            <Layout>
+                <Header className="w-full bg-gray-50 px-0">
+                    <div className="w-full flex items-center justify-between">
+                        <Button
+                            type="text"
+                            icon={
+                                collapsed ? (
+                                    <MenuUnfoldOutlined />
+                                ) : (
+                                    <MenuFoldOutlined />
+                                )
+                            }
+                            onClick={() => setCollapsed(!collapsed)}
+                            style={{
+                                fontSize: "16px",
+                                width: 64,
+                                height: 64,
+                            }}
+                        />
+                        <div className="px-8">
+                            <Dropdown
+                                menu={{
+                                    items: [
+                                        {
+                                            label: (
+                                                <Link
+                                                    href={route("profile.edit")}
+                                                >
+                                                    Profile
+                                                </Link>
+                                            ),
+                                            key: "dashboard",
+                                            icon: <UserOutlined />,
+                                        },
+                                        {
+                                            label: (
+                                                <Link
+                                                    href={route("logout")}
+                                                    method="post"
+                                                >
+                                                    Log out
+                                                </Link>
+                                            ),
+                                            key: "logout",
+                                            icon: <LogoutOutlined />,
+                                        },
+                                    ],
+                                }}
+                                trigger={["click"]}
+                            >
+                                <a onClick={(e) => e.preventDefault()}>
+                                    <Space>
+                                        {auth?.user?.name}
+                                        <DownOutlined />
+                                    </Space>
+                                </a>
+                            </Dropdown>
+                        </div>
+                    </div>
+                </Header>
+                <div className="p-4">
+                    <Breadcrumb>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>{page}</Breadcrumb.Item>
+                    </Breadcrumb>
+                </div>
+                <Content className="mx-4 p-4 bg-gray-50 rounded-lg min-h-[80vh]">
+                    {children}
+                </Content>
+
+                <Footer className="text-center">
+                    Discover Mis Occ ©{new Date().getFullYear()}
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+}
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,3 +1,4 @@
+import UnauthenticatedLayout from "@/Layouts/UnauthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 
 export default function Welcome({ auth }) {
@@ -5,20 +6,22 @@ export default function Welcome({ auth }) {
         <>
             <Head title="Welcome" />
 
-            <div className="h-24 px-4 bg-orange-100 w-full flex items-center justify-between gap-4">
-                <div>
-                    <img src="/images/dmo.png" className="w-16" alt="" />
+            <UnauthenticatedLayout auth={auth}>
+                <div className="h-24 px-4 bg-orange-100 w-full flex items-center justify-between gap-4">
+                    <div>
+                        <img src="/images/dmo.png" className="w-16" alt="" />
+                    </div>
+                    <div>
+                        {auth.user ? (
+                            <Link href={route("dashboard")}>Dashboard</Link>
+                        ) : (
+                            <>
+                                <Link href={route("login")}>Log in</Link>
+                            </>
+                        )}
+                    </div>
                 </div>
-                <div>
-                    {auth.user ? (
-                        <Link href={route("dashboard")}>Dashboard</Link>
-                    ) : (
-                        <>
-                            <Link href={route("login")}>Log in</Link>
-                        </>
-                    )}
-                </div>
-            </div>
+            </UnauthenticatedLayout>
         </>
     );
 }
